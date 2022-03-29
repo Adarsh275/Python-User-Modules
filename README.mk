14fe6fb167d50a03891022f1a212b437ed3145ae
Adarsh275 <adarshaman2000@gmail.com>
2022-03-29 23:04:45 +0530

    added README file

---
+++ README.mk
@@ -0,0 +1,4 @@
+In this project I have created a lot of daily use python modules.
+To acces that use just need to open the web page called as Project_python.
+In that you will get a list of all modules you will get a link in the page.
+You can either look at the module code or download it and modify it & also customize it as it's source code is open.
